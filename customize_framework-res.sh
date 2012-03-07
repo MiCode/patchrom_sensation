0b8f72e3f257753da9601459a4cf8611a312b41a
wangwei_b <wangwei_b@xiaomi.com>
2012-03-07 17:50:13 +0800

    add modify htc resources script
    Change-Id: Id613e5822a02909892b0dc0c2e74770a23809b8d

---
+++ customize_framework-res.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# $1: dir for miui overlay framework-res
+# $2: dir for target framework-res
+#
+
+if [ `basename $1` = "drawable-hdpi" ];then
+	for file in `find "$1"`
+	do
+		newfile=`basename $file`
+		
+		newfile1=`echo $newfile | sed -e "s/^/zhtc_/"`
+		targetfile="$2/drawable-hdpi/$newfile1"
+        	if [ -f $targetfile ]
+        	then
+        	        mkdir -p `dirname $targetfile`
+        	        echo "add miui res: $file"
+			echo "           >> $targetfile"
+        	        cp $file $targetfile
+	       	fi
+
+		newfile1=`echo $newfile | sed -e "s/^/zzz_/"`
+		targetfile="$2/drawable-hdpi/$newfile1"
+        	if [ -f $targetfile ]
+        	then
+        	        mkdir -p `dirname $targetfile`
+    		        echo "add miui res: $file"
+			echo "           >> $targetfile"
+        	        cp $file $targetfile
+	       	fi
+
+	done
+fi
+
